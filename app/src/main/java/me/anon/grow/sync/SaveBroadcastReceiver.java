348091fe85976561ac00332e5369c9e5dee8e621
7LPdWcaW <7LPdWcaW@microgrowery>
2017-01-27 21:32:36 +0000

    Added test broadcast reciever

---
+++ app/src/main/java/me/anon/grow/sync/SaveBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package me.anon.grow.sync;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * // TODO: Add class description
+ */
+public class SaveBroadcastReceiver extends BroadcastReceiver
+{
+	@Override public void onReceive(Context context, Intent intent)
+	{
+		Log.e("test", "Received save request");
+		Log.e("test", intent.getExtras().getString("PLANT_LIST", ""));
+	}
+}
