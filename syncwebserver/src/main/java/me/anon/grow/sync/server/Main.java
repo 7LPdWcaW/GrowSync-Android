b8386d025a4d59c44a4a4ad83db9e7811bf36c8e
7LPdWcaW <7LPdWcaW@microgrowery>
2017-01-28 21:39:41 +0000

    Added server verbose logging

--- syncwebserver/src/main/java/me/anon/grow/sync/server/Main.java
+++ syncwebserver/src/main/java/me/anon/grow/sync/server/Main.java
@@ -15,6 +15,7 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +24,7 @@ public class Main
 {
 	private static String outPath = "./sync/";
 	private static int port = 8420;
+	private static boolean verbose = false;
 
 	public static void main(String[] args) throws Exception
 	{
@@ -30,7 +32,7 @@ public class Main
 		{
 			for (int index = 0; index < args.length; index++)
 			{
-				if ("--path".equalsIgnoreCase(args[index]))
+				if ("-p".equalsIgnoreCase(args[index]) || "--path".equalsIgnoreCase(args[index]))
 				{
 					outPath = args[index + 1];
 				}
@@ -38,16 +40,24 @@ public class Main
 				{
 					port = Integer.parseInt(args[index + 1]);
 				}
+				else if ("-v".equalsIgnoreCase(args[index]) || "--verbose".equalsIgnoreCase(args[index]))
+				{
+					verbose = true;
+				}
 			}
 		}
 
 		new File(outPath).mkdirs();
 
 		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+		log("Server started, listening for requests on port " + port);
+
 		server.createContext("/plants", new HttpHandler()
 		{
 			@Override public void handle(HttpExchange httpExchange) throws IOException
 			{
+				log("New request on /plants");
+
 				if (!httpExchange.getRequestMethod().equalsIgnoreCase("post"))
 				{
 					respond(httpExchange, 400);
@@ -79,115 +89,147 @@ public class Main
 
 			@Override public void handle(HttpExchange httpExchange) throws IOException
 			{
-				if (!httpExchange.getRequestMethod().equalsIgnoreCase("post"))
+				log("New request on /image");
+
+				if (!httpExchange.getRequestMethod().equalsIgnoreCase("post") && !httpExchange.getRequestMethod().equalsIgnoreCase("delete"))
 				{
+					log("Invalid request sent");
 					respond(httpExchange, 400);
 					return;
 				}
 
-				// multipart/form-data, boundary=AaB03x
-				List<String> contentTypes = httpExchange.getRequestHeaders().get("Content-Type");
-				String contentType = null;
-				String boundaryStr = "";
-
-				if (contentTypes != null && contentTypes.size() > 0)
+				if (httpExchange.getRequestMethod().equalsIgnoreCase("post"))
 				{
-					contentType = contentTypes.get(0);
-					if (contentType.startsWith("multipart/form-data;"))
+					// multipart/form-data, boundary=AaB03x
+					List<String> contentTypes = httpExchange.getRequestHeaders().get("Content-Type");
+					String contentType = null;
+					String boundaryStr = "";
+
+					if (contentTypes != null && contentTypes.size() > 0)
 					{
-						String[] params = contentType.split("boundary=");
-						boundaryStr = params[params.length - 1];
-					}
-				}
-
-				if (boundaryStr.equalsIgnoreCase(""))
-				{
-					respond(httpExchange, 400);
-					return;
-				}
-
-				BufferedInputStream inputStream = new BufferedInputStream(httpExchange.getRequestBody(), 8192);
-				byte[] newLine = "\r\n".getBytes();
-				byte[] boundary = boundaryStr.getBytes();
-				byte[] endOfPart = ("\r\n--" + boundaryStr).getBytes();
-				ArrayList<FileData> files = new ArrayList<FileData>();
-
-				while (true)
-				{
-					String firstline = new String(readUntil(inputStream, newLine), "utf-8");
-
-					if (firstline.equals("--" + boundaryStr))
-					{
-						continue;
-					}
-
-					FileData current = new FileData();
-
-					// Because APARENTLY, many http clients DONT send the required boundary field inbetween file
-					// parts.
-					if (firstline.toLowerCase().startsWith("content-disposition"))
-					{
-						InputStream temp = new ByteArrayInputStream(firstline.getBytes());
-						readDisposition(temp, current);
-					}
-					else
-					{
-						readDisposition(inputStream, current);
-					}
-
-					if (firstline.toLowerCase().startsWith("content-type"))
-					{
-						InputStream temp = new ByteArrayInputStream(firstline.getBytes());
-						readContentType(temp, current);
-					}
-					else
-					{
-						readContentType(inputStream, current);
-					}
-
-					current.data = readUntil(inputStream, endOfPart);
-					files.add(current);
-
-					if (current.data.length >= 0)
-					{
-						String currentLine = new String(readUntil(inputStream, newLine), "utf-8");
-
-						// Check if this is the last part.
-						if (currentLine.equals("--"))
+						contentType = contentTypes.get(0);
+						if (contentType.startsWith("multipart/form-data;"))
 						{
-							break;
+							String[] params = contentType.split("boundary=");
+							boundaryStr = params[params.length - 1];
 						}
 					}
+
+					if (boundaryStr.equalsIgnoreCase(""))
+					{
+						log("No boundary for request set");
+						respond(httpExchange, 400);
+						return;
+					}
+
+					BufferedInputStream inputStream = new BufferedInputStream(httpExchange.getRequestBody(), 8192);
+					byte[] newLine = "\r\n".getBytes();
+					byte[] boundary = boundaryStr.getBytes();
+					byte[] endOfPart = ("\r\n--" + boundaryStr).getBytes();
+					ArrayList<FileData> files = new ArrayList<FileData>();
+
+					while (true)
+					{
+						String firstline = new String(readUntil(inputStream, newLine), "utf-8");
+
+						if (firstline.equals("--" + boundaryStr))
+						{
+							continue;
+						}
+
+						FileData current = new FileData();
+
+						// Because APARENTLY, many http clients DONT send the required boundary field inbetween file
+						// parts.
+						if (firstline.toLowerCase().startsWith("content-disposition"))
+						{
+							InputStream temp = new ByteArrayInputStream(firstline.getBytes());
+							readDisposition(temp, current);
+						}
+						else
+						{
+							readDisposition(inputStream, current);
+						}
+
+						if (firstline.toLowerCase().startsWith("content-type"))
+						{
+							InputStream temp = new ByteArrayInputStream(firstline.getBytes());
+							readContentType(temp, current);
+						}
+						else
+						{
+							readContentType(inputStream, current);
+						}
+
+						current.data = readUntil(inputStream, endOfPart);
+						files.add(current);
+
+						if (current.data.length >= 0)
+						{
+							String currentLine = new String(readUntil(inputStream, newLine), "utf-8");
+
+							// Check if this is the last part.
+							if (currentLine.equals("--"))
+							{
+								break;
+							}
+						}
+					}
+
+					// write files to disk
+					FileData pathInfo = null, photo = null;
+					for (FileData file : files)
+					{
+						if ("filename".equalsIgnoreCase(file.name))
+						{
+							pathInfo = file;
+						}
+						else if ("image".equalsIgnoreCase(file.name))
+						{
+							photo = file;
+						}
+					}
+
+					if (pathInfo != null && photo != null)
+					{
+						File file = new File(outPath, new String(pathInfo.data, "utf-8"));
+						new File(file.getParent()).mkdirs();
+
+						FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+						log("Writing file to: " + file.getAbsolutePath());
+
+						long total = photo.data.length;
+						long current = 0;
+						int percent = 0;
+						for (byte b : photo.data)
+						{
+							fileOutputStream.write(b);
+							current++;
+
+							if (((double)current / (double)total) * 100 > percent)
+							{
+								percent++;
+								log("Writing file " + percent + "%");
+							}
+						}
+
+						fileOutputStream.flush();
+						fileOutputStream.close();
+					}
 				}
-
-				// write files to disk
-				FileData pathInfo = null, photo = null;
-				for (FileData file : files)
+				else if (httpExchange.getRequestMethod().equalsIgnoreCase("delete"))
 				{
-					if ("filename".equalsIgnoreCase(file.name))
+					String[] params = httpExchange.getRequestURI().getQuery().split("&");
+					for (String param : params)
 					{
-						pathInfo = file;
+						if (param.startsWith("image"))
+						{
+							String[] kv = param.split("=");
+							boolean delete = new File(outPath, kv[1]).delete();
+							log("File " + new File(outPath, kv[1]).getAbsolutePath() + " deleted " + (delete ? "" : "un") + "successfully");
+						}
 					}
-					else if ("image".equalsIgnoreCase(file.name))
-					{
-						photo = file;
-					}
-				}
-
-				if (pathInfo != null && photo != null)
-				{
-					File file = new File(outPath, new String(pathInfo.data, "utf-8"));
-					new File(file.getParent()).mkdirs();
-
-					FileOutputStream fileOutputStream = new FileOutputStream(file);
-
-					for (byte b : photo.data)
-					{
-						fileOutputStream.write(b);
-					}
-
-					fileOutputStream.flush();
-					fileOutputStream.close();
 				}
 
 				respond(httpExchange, 200);
@@ -283,13 +325,24 @@ public class Main
 		server.start();
 	}
 
+	private static void log(String message)
+	{
+		if (verbose)
+		{
+			Date date = new Date();
+			System.out.println("[" + date.toLocaleString() + "] " + message);
+		}
+	}
+
 	private static void respond(HttpExchange httpExchange, int errorCode)
 	{
 		try
 		{
+			log("Writing " + errorCode + " response {\"success\": " + (errorCode == 200) + "}");
+
 			httpExchange.sendResponseHeaders(errorCode, 0);
 			OutputStream os = httpExchange.getResponseBody();
-			os.write(0);
+			os.write(("{\"success\": " + (errorCode == 200) + "}").getBytes());
 			os.close();
 		}
 		catch (IOException e)
